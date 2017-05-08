d82c2b48d978985e64b1c04656e225b774915071
Koichi Shiraishi <zchee.io@gmail.com>
2017-05-09 00:08:48 +0900

    doc: add project description
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The gmedium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command gmedium implements command line tool for Medium stories.
+package main
